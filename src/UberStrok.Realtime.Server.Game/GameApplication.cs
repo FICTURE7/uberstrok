58a125dd9ff06a838adbc9fbcfc44eb0239597d1
FICTURE7 <FICTURE7@gmail.com>
2018-01-08 13:28:15 +0400

    add: configured log4net for game server fix: log4net config of comm server

--- src/UberStrok.Realtime.Server.Game/GameApplication.cs
+++ src/UberStrok.Realtime.Server.Game/GameApplication.cs
@@ -1,17 +1,31 @@
-﻿using Photon.SocketServer;
+﻿using log4net;
+using log4net.Config;
+using Photon.SocketServer;
+using System.IO;
 
 namespace UberStrok.Realtime.Server.Game
 {
     public class GameApplication : ApplicationBase
     {
+        private static readonly ILog Log = LogManager.GetLogger(typeof(GameApplication));
+
         protected override PeerBase CreatePeer(InitRequest initRequest)
         {
+            Log.Info($"Accepted new connection at {initRequest.RemoteIP}:{initRequest.RemotePort}");
+
             return new GamePeer(initRequest);
         }
 
         protected override void Setup()
         {
-            // Space
+            // Add a the log path to the properties so can use them in log4net.config.
+            GlobalContext.Properties["Photon:ApplicationLogPath"] = Path.Combine(ApplicationPath, "log");
+            // Configure log4net to use log4net.config file.
+            var configFile = new FileInfo(Path.Combine(BinaryPath, "log4net.config"));
+            if (configFile.Exists)
+                XmlConfigurator.ConfigureAndWatch(configFile);
+
+            Log.Info("Started CommServer...");
         }
 
         protected override void TearDown()
