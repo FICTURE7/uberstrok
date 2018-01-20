b5c999a03e43e6757e68c340d945042d75c1ec03
FICTURE7 <FICTURE7@gmail.com>
2018-01-20 20:17:13 +0400

    add: scoreboard

--- src/UberStrok.Realtime.Server.Game/TeamDeathMatchGameRoom.cs
+++ src/UberStrok.Realtime.Server.Game/TeamDeathMatchGameRoom.cs
@@ -1,4 +1,5 @@
 ﻿using log4net;
+using UberStrok.Core.Common;
 using UberStrok.Core.Views;
 
 namespace UberStrok.Realtime.Server.Game
@@ -10,8 +11,28 @@ namespace UberStrok.Realtime.Server.Game
         public TeamDeathMatchGameRoom(GameRoomDataView data) : base(data)
         {
             // Space
-        } 
+        }
 
+        public int BlueTeamScore { get; set; }
+        public int RedTeamScore { get; set; }
 
+        protected override void OnPlayerKilled(PlayerKilledEventArgs args)
+        {
+            base.OnPlayerKilled(args);
+
+            foreach (var player in Players)
+            {
+                if (player.Actor.Cmid != args.AttackerCmid)
+                    continue;
+
+                if (player.Actor.Team == TeamID.BLUE)
+                    BlueTeamScore++;
+                else if (player.Actor.Team == TeamID.RED)
+                    RedTeamScore++;
+            }
+
+            foreach (var player in Players)
+                player.Events.Game.SendUpdateRoundScore(RoundNumber, (short)BlueTeamScore, (short)RedTeamScore);
+        }
     }
 }
