715bb911534e3d9e461ac146abfad55de15f407a
FICTURE7 <ficture7@gmail.com>
2018-06-14 18:21:50 +0400

    fix: some errs

--- tests/UberStrok.Realtime.Server.Game.Tests/Program.cs
+++ tests/UberStrok.Realtime.Server.Game.Tests/Program.cs
@@ -1,17 +1,19 @@
 ﻿namespace UberStrok.Realtime.Server.Game.Tests
 {
-    /*
     public static class Program
     {
         public static void Main()
         {
+            /*
             var peer = new TestPeer(null);
 
             peer.Handlers.Add(new GamePeerOperationHandler());
             peer.Handlers.Add(new TeamDeathMatchGameRoom(null));
+            */
         }
     }
-
+    
+    /*
     public class TestPeer : Peer
     {
         public TestPeer(InitRequest request) : base(request)
