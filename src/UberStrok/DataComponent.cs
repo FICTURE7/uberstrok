0105fabfbd4ac5c895d58421b185d713fa0f4b77
FICTURE7 <ficture7@gmail.com>
2018-07-11 20:46:37 +0400

    engine: add DataComponent

---
+++ src/UberStrok/DataComponent.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UberStrok
+{
+    public abstract class DataComponent : Component
+    {
+        protected sealed override void OnUpdate()
+        {
+            /* DataComponent holds data only and does not need to be updated. */
+        }
+    }
+}
--- src/UberStrok/Transform.cs
+++ src/UberStrok/Transform.cs
@@ -1,13 +1,8 @@
 ﻿namespace UberStrok
 {
-    public class Transform : Component
+    public class Transform : DataComponent
     {
         public Vector3 Rotation { get; set; }
         public Vector3 Position { get; set; }
-
-        protected override void OnUpdate()
-        {
-            // Space
-        }
     }
 }
