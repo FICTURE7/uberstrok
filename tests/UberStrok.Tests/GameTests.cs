f95d8eb24e9aac787578605f3828698a0ba1982d
FICTURE7 <ficture7@gmail.com>
2018-07-13 17:21:51 +0400

    engine: add OnCommand tests

--- tests/UberStrok.Tests/GameTests.cs
+++ tests/UberStrok.Tests/GameTests.cs
@@ -66,6 +66,25 @@ namespace UberStrok.Tests
         }
 
         [Test]
+        public void OnCommand()
+        {
+            var game = new Game();
+            var command = new MockCommand();
+
+            game.DoTick();
+            game.OnCommand(command);
+
+            Assert.That(command.Tick, Is.EqualTo(game.Tick));
+        }
+
+        [Test]
+        public void OnCommand_Null_Args_Exception()
+        {
+            var game = new Game();
+            Assert.That(() => game.OnCommand(null), Throws.ArgumentNullException);
+        }
+
+        [Test]
         public void State_()
         {
             var game = new Game();
