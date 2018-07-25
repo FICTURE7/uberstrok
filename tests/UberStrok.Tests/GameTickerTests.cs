819e341b37357665ad76aa511fdadcb31fe703af
FICTURE7 <ficture7@gmail.com>
2018-07-25 18:17:37 +0400

    engine: some tests

---
+++ tests/UberStrok.Tests/GameTickerTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace UberStrok.Tests
+{
+    [TestFixture]
+    public class GameTickerTests
+    {
+        [Test]
+        public void Ctor_tps_Less_Than_0_Exception()
+        {
+            Assert.That(() => new GameTicker(-1), Throws.InstanceOf<ArgumentOutOfRangeException>());
+        }
+
+        [Test]
+        public void Sandbox()
+        {
+            var game = new Game();
+            var ticker = new GameTicker(10);
+        }
+    }
+}
