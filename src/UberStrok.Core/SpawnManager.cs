8216e9c0e600bb6d0e0c66be2deed7f62843e683
FICTURE7 <ficture7@gmail.com>
2019-04-19 13:47:33 +0400

    game/fix: reworked spawn manager
    - reworked spawn manager to use seperate indices for every team

--- src/UberStrok.Core/SpawnManager.cs
+++ src/UberStrok.Core/SpawnManager.cs
@@ -6,21 +6,20 @@ namespace UberStrok.Core
 {
     public class SpawnManager
     {
-        private int _index;
-        private int _spawnCount;
-
         /* Randomizer we're going to use to spawn stuff. */
         private readonly Random _rand;
         /* List of spawn points of the players in respective teams. */
-        private readonly Dictionary<TeamID, List<SpawnPoint>> _spawnPoints;
+        private readonly Dictionary<TeamID, List<SpawnPoint>> _points;
+        private readonly Dictionary<TeamID, int> _indices;
 
         public SpawnManager()
         {
             _rand = new Random();
-            _spawnPoints = new Dictionary<TeamID, List<SpawnPoint>>();
+            _indices = new Dictionary<TeamID, int>();
+            _points = new Dictionary<TeamID, List<SpawnPoint>>();
         }
 
-        public bool IsLoaded(TeamID team) => _spawnPoints.ContainsKey(team);
+        public bool IsLoaded(TeamID team) => _points.ContainsKey(team);
 
         public void Load(TeamID team, List<Vector3> positions, List<byte> rotations)
         {
@@ -34,20 +33,23 @@ namespace UberStrok.Core
             for (int i = 0; i < length; i++)
                 spawns.Add(new SpawnPoint(positions[i], rotations[i]));
 
-            _spawnPoints[team] = spawns;
-            _index = _rand.Next(_spawnPoints.Count);
+            _points[team] = spawns;
+            _indices[team] = _rand.Next(_points.Count);
         }
 
         public SpawnPoint Get(TeamID team)
         {
-            /* Slightly less random spawns. */
-            if (_spawnCount % _spawnPoints.Count == 0)
-                _index = _rand.Next(_spawnPoints.Count);
-            else
-                _index++;
+            /* Incase stuff goes loose. */
+            if (!_indices.ContainsKey(team) || !_points.ContainsKey(team))
+                return default(SpawnPoint);
 
-            _spawnCount++;
-            return _spawnPoints[team][_index];
+            var point = _points[team][_indices[team]++];
+            var index = _indices[team];
+
+            if (index % _points[team].Count == 0)
+                _indices[team] = _rand.Next(_points.Count);
+
+            return point;
         }
     }
 }
